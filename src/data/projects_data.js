a533f19b4368b1046edd931e385df3c1cd7bb7bb
João Felipe P <jf.pelliccione@gmail.com>
2022-03-29 13:38:14 -0300

    Inserção de novo projeto.

--- src/data/projects_data.js
+++ src/data/projects_data.js
@@ -171,6 +171,15 @@ const projects = {
       startDate: '11/03/2022',
       timeInProg: '7 months and 27 days',
     },
+    {
+      id: 'CRUDApiTypescript',
+      title: 'RESTful Typescript API',
+      description: 'Development of a RESTful CRUD API with endpoints aiming the reading and editing of a MySQL database. To structure the respective project there were use of Node.JS, Express and Typescript.',
+      skills: 'Node.JS & Typescript',
+      URL: 'https://github.com/joaofelipelliccione/joaofelipelliccione.github.io/tree/main/projetos-trybe/3-Backend/07_NodeJs-CRUD-API-Typescript',
+      startDate: '29/03/2022',
+      timeInProg: '8 months and 17 days',
+    },
   ],
 };
 
